e552d381af3cb63f49be3a98d1795d8e0ea67c05
lucasmercau <lucas.mercau99@gmail.com>
2023-11-08 14:43:32 -0300

    dark mode

---
+++ scripts/mode.js
@@ -0,0 +1,30 @@
+const modeButton = document.querySelector("#mode");
+const main = document.querySelector("main");
+const sections = document.querySelectorAll(".card");
+const hr = document.querySelector("hr");
+const link = document.querySelectorAll(".homework li a");
+
+modeButton.addEventListener("click", () => {
+	if (modeButton.textContent.includes("🕶️")) {
+		main.style.background = "gray";
+		main.style.color = "#fff";
+        sections.forEach(section => {
+            section.style.background = "black";
+        });
+        link.forEach(a => {
+            a.style.color = "#8181F7";
+        });
+		modeButton.textContent = "🔆";
+	} else {
+        main.style.background = "white";
+		main.style.color = "#000";
+        hr.style.color = "black";
+        sections.forEach(section => {
+            section.style.background = "white";
+        });
+        link.forEach(a => {
+            a.style.color = "blue";
+        });
+		modeButton.textContent = "🕶️";
+	}
+});
