88524eff28fa3bf41f3068aa4d2b127c80fda97d
lucasmercau <lucas.mercau99@gmail.com>
2023-11-08 10:44:30 -0300

    Dom Manipulation

---
+++ week02/scripts/bom.js
@@ -0,0 +1,31 @@
+const input = document.querySelector("#favchap");
+const button = document.querySelector("button");
+const list = document.querySelector("#list");
+
+button.addEventListener("click", () => {
+    if (input.value != "")
+    {
+        const li = document.createElement('li');
+        const deleteButton = document.createElement('button');
+        li.textContent = input.value;
+        deleteButton.textContent = "❌";
+        li.append(deleteButton);
+        list.appendChild(li);
+        input.focus();
+        input.value = "";   
+        deleteButton.addEventListener("click", () => {
+            list.removeChild(li);
+            input.focus();
+            input.value = "";            
+        })
+    }
+    else 
+    {
+        input.focus();
+    }
+});
+
+
+input.focus();
+input.value = "";
+
