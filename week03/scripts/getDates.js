61d838944bb57da0ad503b23d5b8592d05e6ca3e
lucasmercau <lucas.mercau99@gmail.com>
2023-11-13 17:54:08 -0300

    responsive img

---
+++ week03/scripts/getDates.js
@@ -0,0 +1,18 @@
+// let currentYear = new Date().getFullYear();
+// document.getElementById("currentYear").textContent = currentYear;
+
+const options = {
+    year: 'numeric',
+    month: '2-digit',
+    day: '2-digit',
+    hour: '2-digit',
+    minute: '2-digit',
+    second: '2-digit',
+    hour12: false,
+};
+
+const lastModification = new Date(document.lastModified);
+let formattedDate = lastModification.toLocaleString("en-Us", options);
+formattedDate = formattedDate.replace(',', '');
+
+document.getElementById("lastModified").textContent = `Last Modification: ${formattedDate}`;
