4a0d913eac42131be01d8eaa25ef6bf3c5f0f240
lucasmercau <lucas.mercau99@gmail.com>
2023-11-29 13:03:02 -0300

    Team Activity - JSON and the Weather API

---
+++ scripts/links.js
@@ -0,0 +1,23 @@
+const baseURL = "https://lucasmercau.github.io/wdd230/";
+const linksURL = "https://lucasmercau.github.io/wdd230/links.json";
+const ul = document.querySelector(".homework");
+
+async function getLinks() {
+    const response = await fetch(linksURL);
+    const data = await response.json();
+    displayLinks(data.weeks);
+}
+
+getLinks();
+                   
+function displayLinks(weeks) {
+    weeks.forEach(weekindex => {
+        const li = document.createElement("li");
+        li.innerHTML = `${weekindex.week}:`
+        weekindex.links.forEach(link => {
+            li.innerHTML += ` <a href="${link.url}">${link.title}</a> |`
+        })
+        li.innerHTML = li.innerHTML.slice(0, -1);
+        ul.appendChild(li);
+    });
+}
